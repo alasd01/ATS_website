9c40fd96532957556fa7d2001cec24395ad7db75
David <alasdavid01@gmail.com>
2022-12-03 16:12:21 -0600

    v3

---
+++ initialize.js
@@ -0,0 +1,48 @@
+var ATS_list = [
+"SJH",
+"RRH",
+"BMA",
+"BMS",
+"ADH",
+"TSG",
+"ETC",
+"CS5",
+"MEZ",
+"HRH",
+"HRC",
+"CS6",
+"EPS",
+"BRB",
+"JES",
+"COM",
+"GAR",
+"WIN",
+"DFF",
+"ERC",
+"TRG",
+"HLB",
+"HTB",
+"HDB",
+"GDC",
+"FNT",
+"PMA",
+"NMS",
+"CPE",
+]
+function initialize(){
+    var sorted_list = ATS_list.sort();
+    var table = document.getElementById("table");
+    for(let i = 1; i < sorted_list.length; i++){
+        var ATS_name = sorted_list[i-1]
+        var row = table.insertRow(i);
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = ATS_name;
+        var cell2 = row.insertCell(1);
+        cell2.setAttribute('id',ATS_name+"_avail");
+        var cell3 = row.insertCell(2);
+        cell3.setAttribute('id',ATS_name+"_status");
+    }
+
+}
+
+initialize();
--- mqtt.js
+++ mqtt.js
@@ -1,14 +1,5 @@
 client = new Paho.MQTT.Client("5ff471d504b74f45b49c7c48b0ccf558.s2.eu.hivemq.cloud", 8884,"clientId");
 //client = new Paho.MQTT.Client("iot.eclipse.org", Number(80), "/ws", "clientId");
-var CS6_avail = document.getElementById('CS6_avail');
-var CS6_status = document.getElementById('CS6_status');
-var GRE_avail = document.getElementById('GRE_avail');
-var GRE_status = document.getElementById('GRE_status');
-var test_avail = document.getElementById('test_avail');
-var test_status = document.getElementById('test_status');
-var ATS_name
-var color = document.getElementById('table').getElementsByTagName("td");
-console.log("hello");
 
 //set callback handlers
 client.onConnectionLost = function (responseObject) {
@@ -18,7 +9,7 @@ client.onConnectionLost = function (responseObject) {
 
 // Called when the connection is made
 function onConnect(){
-	console.log("Test");
+	console.log("Connected");
   client.subscribe("#");
 }
 
@@ -33,63 +24,34 @@ client.connect({
 
 
 client.onMessageArrived = function (message) {
-  console.log("Message Arrived: " + message.payloadString);
-  var mess = message.destinationName;
-  var sub = mess.substring(0,mess.indexOf('/'));
-  ATS_name = sub;
-  var topic = mess.substring(mess.indexOf('/')+1);
+    var mess = message.destinationName;
+   // var loc = mess.substring(3,mess.indexOf('/'));
+    var loc = mess.substring(4,7);
+    var topic = mess.substring(mess.indexOf('/')+1);
+    var ATS_avail = document.getElementById(loc+"_avail");
+    var ATS_status = document.getElementById(loc+"_status");
 
-  if (sub==="ATS_CS6"){
-    if(topic==="availability"){
-        CS6_avail.innerHTML = message.payloadString + " | " + message.destinationName;
-        CS6_avail.style.backgroundColor="#90EE90";
-        if(message.payloadString === "Not Available"){
-          sendmail();
-          CS6_avail.style.backgroundColor="#ff0000";
-        }
-    }
-    else{
-      CS6_status.innerHTML = message.payloadString + " | " + message.destinationName;
-      CS6_status.style.backgroundColor="#90EE90";
-      if(message.payloadString === "Disconnected"){
-        sendmail();
-        CS6_status.style.backgroundColor="#ff0000";
-      }
-    }
-  }
 
-  if (sub==="ATS_GRE"){
     if(topic==="availability"){
-        GRE_avail.innerHTML = message.payloadString + " | " + message.destinationName;
-        GRE_avail.style.backgroundColor="#90EE90";
+        ATS_avail.innerHTML = message.payloadString + " | " + message.destinationName;
+        ATS_avail.style.backgroundColor="#90EE90";
         if(message.payloadString === "Not Available"){
-          sendmail();
-          GRE_avail.style.backgroundColor="#ff0000";
+            sendmail();
+            ATS_avail.style.backgroundColor="#ff0000";
         }
-    }
-    else{
-      GRE_status.innerHTML = message.payloadString + " | " + message.destinationName;
-      GRE_status.style.backgroundColor="#90EE90";
-      if(message.payloadString === "Disconnected"){
-        sendmail();
-        GRE_status.style.backgroundColor="#ff0000";
-      }
-    }
-    
-  }
-  
-  if(sub==="TEST"){
-    if(topic==="availability"){
-      test_avail.innerHTML = message.payloadString + " | " + message.destinationName;
-      if(message.payloadString === "Disconnected"){
-        sendmail();
-      }
-    }
-    else{
-      test_status.innerHTML = message.payloadString + " | " + message.destinationName;
-    
-    }
-  }
-  
-  
 }
+    if(topic==="pi_status"){
+        ATS_status.innerHTML = message.payloadString + " | " + message.destinationName;
+        ATS_status.style.backgroundColor="#90EE90";
+        if(message.payloadString === "Disconnected"){
+            sendmail();
+            ATS_status.style.backgroundColor="#ff0000";
+        }
+    }
+}
+
+
+
+
+
+  
