53b0a979fcbf447e1dc1d593bb04221d97116e88
David <alasdavid01@gmail.com>
2023-02-03 12:28:37 -0600

    old

--- mqtt2.js
+++ mqtt2.js
@@ -1,4 +1,3 @@
-
 var clientId = Math.floor(Math.random() * 10000).toString();
 client = new Paho.MQTT.Client("5ff471d504b74f45b49c7c48b0ccf558.s2.eu.hivemq.cloud", 8884, clientId);
 var ATS_name
@@ -27,7 +26,6 @@ client.connect({
 });
 
 
-
 client.onMessageArrived = function (message) {
   // time
   var today = new Date();
@@ -35,42 +33,46 @@ client.onMessageArrived = function (message) {
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   var dateTime = time +' &bull; '+ date;
 
-  
- //topic/message+
+    
     var mess = message.destinationName;
-    var index = message.payloadString.indexOf('/');
    // var loc = mess.substring(3,mess.indexOf('/'));
     var loc = mess.substring(4,7);
-    var time = message.payloadString.substring(index+1);
     var topic = mess.substring(mess.indexOf('/')+1);
     ATS_name = loc;
+
+    //local time
+    // var old_mess = localStorage.getItem(loc+"mess");
+    // if(topic===old_mess){
+    //   dateTime = localStorage.getItem(loc+"time");
+    // }
+    // if(topic != old_mess){
+    //   localStorage.setItem(loc+"mess",topic);
+    //   localStorage.setItem(loc+"time",dateTime)
+    // }
+
+
     if(ATS_list.includes(loc))
     {
       var ATS_avail = document.getElementById(loc+"_avail");
       var ATS_status = document.getElementById(loc+"_status");
-      //window.localStorage.getItem(ATS_name+'_time')
-
       var ATS_time = document.getElementById(loc+"_time");
-      ATS_time.innerHTML = time;
+      ATS_time.innerHTML = dateTime;
       //ATS_time.style.backgroundColor="#90EE90";
       
-      
-
       if(topic==="availability"){
-          ATS_avail.innerHTML = message.payloadString.substring(0,index);
+          ATS_avail.innerHTML = message.payloadString;// + " | " + message.destinationName; //+ " | Updated on "  +  dateTime;
           ATS_avail.style.backgroundColor="#90EE90";
 
-          if(message.payloadString.substring(0,index) === "Not Available"){
+          if(message.payloadString === "Not Available"){
               email_message = "Emergency Backup is Not Available at";
               //sendmail();
               ATS_avail.style.backgroundColor="#ff0000";
           }
     }
       if(topic==="pi_status"){
-          ATS_status.innerHTML = message.payloadString.substring(0,index);//+ " | " + message.destinationName;
+          ATS_status.innerHTML = message.payloadString ;//+ " | " + message.destinationName;
           ATS_status.style.backgroundColor="#90EE90";
-          
-          if(message.payloadString.substring(0,index) === "Disconnected"){
+          if(message.payloadString === "Disconnected"){
               email_message = "The Raspberry Pi is Disconnected at";
               //sendmail();
               ATS_status.style.backgroundColor="#ff0000";
@@ -78,9 +80,3 @@ client.onMessageArrived = function (message) {
       }
   }
 }
-
-
-
-
-
-  
